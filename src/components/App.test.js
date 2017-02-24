54a80342c563adb26a5ae848bfadbc8fd6e1adea
Daniel Kuroski <daniel.kuroski@gmail.com>
2017-02-24 19:39:06 -0300

    Added describe on tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import App from './App';
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const AppTree = renderer.create(
-    <App />
-  ).toJSON();
-  expect(AppTree).toMatchSnapshot();
-});
+describe('App', () => {
+  it('renders correctly', () => {
+    const AppTree = renderer.create(
+      <App />
+    ).toJSON();
+    expect(AppTree).toMatchSnapshot();
+  });
+})
--- src/components/bookstore/BookStore.test.js
+++ src/components/bookstore/BookStore.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import BookStore from './BookStore';
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const BookStoreTree = renderer.create(
-    <BookStore />
-  ).toJSON();
-  expect(BookStoreTree).toMatchSnapshot();
-});
+describe('BookStore', () => {
+  it('renders correctly', () => {
+    const BookStoreTree = renderer.create(
+      <BookStore />
+    ).toJSON();
+    expect(BookStoreTree).toMatchSnapshot();
+  });
+})
