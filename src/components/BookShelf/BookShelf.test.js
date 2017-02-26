6bdca66fb095e473f92cbdea9012197ad53f7317
Daniel Kuroski <kuroski@Daniels-Mac-mini.local>
2017-02-26 14:35:07 -0300

    Added books render tests to BookStore

--- src/components/BookShelf/BookShelf.test.js
+++ src/components/BookShelf/BookShelf.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import BookShelf from './'
 import renderer from 'react-test-renderer'
+import {shallow} from 'enzyme'
+import booksJson from './__mocks__/books.json'
 
 describe('BookShelf', () => {
   it('renders correctly', () => {
@@ -8,5 +10,20 @@ describe('BookShelf', () => {
       <BookShelf />
     ).toJSON()
     expect(BookShelfTree).toMatchSnapshot()
+  })
+
+  it('should render zero books', () => {
+    const wrapper = shallow(<BookShelf books={[]} />)
+    expect(wrapper.find('li')).toHaveLength(0)
+  })
+
+  it('should render undefined books', () => {
+    const wrapper = shallow(<BookShelf books={undefined} />)
+    expect(wrapper.find('li')).toHaveLength(0)
+  })
+
+  it('should render some books', () => {
+    const wrapper = shallow(<BookShelf books={booksJson} />)
+    expect(wrapper.find('li')).toHaveLength(2)
   })
 })
