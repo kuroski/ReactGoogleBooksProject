7e71b47caa59095ca9cad4e4bd320acb71e4c226
Daniel Kuroski <daniel.kuroski@gmail.com>
2017-02-27 12:28:27 -0300

    Changed book results from 15 to 10

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,7 @@ export function all(term, page = 0) {
     return request.get(`${API_URL}/volumes`)
         .query({
             q: term,
-            maxResults: 15,
+            maxResults: 10,
             startIndex: page
         }).then(result => {
             return result.body
--- src/test/__mocks__/constants.js
+++ src/test/__mocks__/constants.js
@@ -1,4 +1,5 @@
+const PER_PAGE = 10
 export const API_URL = 'https://www.googleapis.com/books/v1'
-export const HARRY_POTTER_URL = '/volumes?q=Harry%20Potter&maxResults=15&startIndex=0'
-export const HARRY_POTTER_URL_SECOND_PAGE = '/volumes?q=Harry%20Potter&maxResults=15&startIndex=1'
-export const BOOKS_PER_PAGE = 15
+export const HARRY_POTTER_URL = `/volumes?q=Harry%20Potter&maxResults=${PER_PAGE}&startIndex=0`
+export const HARRY_POTTER_URL_SECOND_PAGE = `/volumes?q=Harry%20Potter&maxResults=${PER_PAGE}&startIndex=1`
+export const BOOKS_PER_PAGE = PER_PAGE
