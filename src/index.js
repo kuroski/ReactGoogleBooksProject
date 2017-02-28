99756b4de3a5db790da9ff3cd2e6822f0c1af1ea
Daniel Kuroski <daniel.kuroski@gmail.com>
2017-02-28 00:17:09 -0300

    Added MUI component library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom'
 import router from './router'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import './index.css'
+
+injectTapEventPlugin()
 
 ReactDOM.render(
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Router, Route, IndexRoute, browserHistory} from 'react-router'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import App from '../components/App'
 import BookStore from '../components/BookStore'
 import BookDetail from '../components/BookDetail'
@@ -11,13 +12,15 @@ addLocaleData([...pt,])
 
 const router = (
     <IntlProvider locale="pt" messages={localeData}>
-        <Router history={browserHistory}>
-            <Route path="/" component={App}>
-                <IndexRoute component={BookStore} />
-                <Route path="/:bookId" component={BookDetail} />
-                <Route path="*" component={BookStore}/>
-            </Route>
-        </Router>
+        <MuiThemeProvider>
+            <Router history={browserHistory}>
+                <Route path="/" component={App}>
+                    <IndexRoute component={BookStore} />
+                    <Route path="/:bookId" component={BookDetail} />
+                    <Route path="*" component={BookStore}/>
+                </Route>
+            </Router>
+        </MuiThemeProvider>
     </IntlProvider>
 )
 
